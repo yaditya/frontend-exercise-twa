8d45dbbf5046397afb0ed8ea7d727f0a92ec38b2
Yovi Budirahardjo <yovi.budirahardjo@optus.com.au>
2014-08-28 20:41:25 +1000

    renamed style.css to app.css for Foundation override, updated JS and html

--- js/main.js
+++ js/main.js
@@ -1,17 +1,73 @@
 'use strict';
 
+/**
+ * this method loads the data.json and returns data object with a promise method
+ * @return {object} data object
+ */
+function loadData () {
+	return $.getJSON('js/data.json').then(function (data) {
+		return data.data;
+	});
+
+}
+
+function runProgress () {
+	loadData().done(function (data) {
+
+		var start = data.lightbox.start,
+			finish = data.lightbox.finish,
+			duration = data.lightbox.duration;
+
+		// reset the progress
+		// passing initial start data
+		resetData(start);
+
+		// trigger the foundation modal
+		$('#myModal').foundation('reveal', 'open');
+
+		// start animating the progress bar
+		$('#bar')
+	    .animate({
+	    	// animate the width
+	        width: finish + "%"
+	    }, {
+	        duration: duration,
+	        step: function( now, fx ) {
+	        	// update the progress percentage for each step
+	            $('#progress').text(Math.ceil(now));
+	        },
+	        complete: function () {
+	        	// when the animation completes, show the complete text
+	            $('#progress-container').addClass('hide');
+	            $('#completed-container').removeClass('hide');
+	            $('#bar').addClass('completed');
+	        }
+	    });
+	});
+}
+
+function resetData(startValue) {
+	var start = startValue || 0;
+
+	// make sure start value is not negative
+	if (start < 0) {
+		start = 0;
+	}
+
+	$('#bar').css({
+		width: start
+	});
+
+	$('#progress-container').removeClass('hide');
+	$('#completed-container').addClass('hide');
+	$('#bar').removeClass('completed');
+}
+
 $(function() {
-	$("#bar")
-    .animate({
-        width: "100%"
-    }, {
-        duration: 2000,
-        step: function( now, fx ) {
-            $('#progress').text(Math.ceil(now));
-        },
-        complete: function () {
-            $('p').html('This task is 100% completed');
-            $('#bar').css('background-color', 'green');
-        }
-    });
+	runProgress();
+
+	$('#reset').click(function () {
+		resetData();
+		runProgress();
+	});
 });
